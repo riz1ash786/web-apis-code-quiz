53d768726cbba29deade684e9d1ca98501d2a8b1
Rizwan Ashraf <riz1ash786@gmail.com>
2022-01-14 12:12:03 +0000

    updating variables and classes

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,30 @@
-// var questionEl = document.getElementById("question");
-// var answersEl = document.getElementById("questions");
-// var startQuizButtonEl = document.getElementById("startQuiz");
+var quiz = document.querySelector(".quiz");
+var button = document.querySelector(".quizStartButton");
+var quizInfo = document.querySelector(".quizCard");
+var answers_container = document.querySelector(".answerBox");
+var check = document.querySelectorAll(".answerBoxOptions");
+var quiz_question = document.querySelector("h2");
+var question_number = document.querySelector("h3");
+var image = document.querySelector(".quizImage");
+var resultDescription = document.querySelector(".quizResults");
 
-var resultDescription = document.querySelector(".quiz-results");
-var answers_container = document.querySelector(".answers-container");
-var StartButton = document.querySelector(".start-btn");
-var cardContents = document.querySelector(".card");
-var Answers = document.querySelectorAll(".answers-container-answer");
-var quizQuestions = document.querySelector("h2");
+let currentQuestion = 0;
+let Score = 0;
+
+var questions = [
+  {
+    question: "What does HTML stand for?",
+    answers: ["Hypertext Markup Language", "Blah", "Blah2"],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: "Who is the true formula1 champion of 2021?",
+    answers: ["Hamilton", "Lewis Hamilton", "Lewis", "Team44"],
+    correctAnswerIndex: 2,
+  },
+  {
+    question: "How many days are there in week?",
+    answers: ["4", "8", "5", "7"],
+    correctAnswerIndex: 3,
+  },
+];
