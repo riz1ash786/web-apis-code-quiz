f666273396f9cf4f98cb34e67e5eda81c9c214b1
Rizwan Ashraf <riz1ash786@gmail.com>
2022-01-14 13:05:52 +0000

    render next question, issues regarding score

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,9 @@ var answers_container = document.querySelector(".answerBox");
 var check = document.querySelectorAll(".answerBoxOptions");
 var quiz_question = document.querySelector("h2");
 var question_number = document.querySelector("h3");
+var quiz_score = document.querySelector("h2");
+var question_score = document.querySelector("h3");
+var question_score = document.querySelector("h3");
 var image = document.querySelector(".quizImage");
 var resultDescription = document.querySelector(".quizResults");
 
@@ -28,3 +31,21 @@ var questions = [
     correctAnswerIndex: 3,
   },
 ];
+button.addEventListener("click", () => {
+  quizInfo.style.display = "none";
+  button.style.display = "none";
+  quiz.style.display = "block";
+  renderNextQuestion(currentQuestion);
+});
+var clearQuestion = () => {
+  quiz_question.textContent = "";
+  answers_container.innerHTML = "";
+};
+var renderNextQuestion = (i) => {
+  question_number.textContent = `Question ${i + 1}/3`;
+  quiz_question.textContent = questions[i].question;
+};
+var renderNextQuestion = (i) => {
+  question_score.textContent = `Score ${i + 1}/3`;
+  quiz_score.textContent = questions[i].question;
+};
